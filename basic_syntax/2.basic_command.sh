dc808421575f7186805e73c1e3733aa7657052c6
(hyungkey) <(hyungkey0601@gmail.com)>
2025-06-04 11:48:00 +0900

    11:47

--- basic_syntax/2.basic_command.sh
+++ basic_syntax/2.basic_command.sh
@@ -30,5 +30,16 @@ git checkout 커밋ID
 # 특정 branch로의 전환
 git checkout 브랜치명
 
+# pull은 원격변경사항을 로컬로 내려받는 것(fetch+merge)
+git pull origin main
+
+# fetch는 변경사항을 local가져오 되, 병합은 하지 않은 것.
+git fetch origin main
+# 먄약 모든 브랜치의 변경사항을 가져오려면
+git fetch --all
+# 버전별(commit간) 비교 명령어
+git diff commitID(A) commitID(B)
+git diff commitID1 commitID2
+git diff 브랜치1 브랜치2 # 많이 쓰임
 
 
