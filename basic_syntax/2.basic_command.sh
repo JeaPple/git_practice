26a5ffe5ffb7c604955a90590ae9f07eee59c497
(hyungkey) <(hyungkey0601@gmail.com)>
2025-06-04 09:36:15 +0900

    메시지 제목 1.회원가입 2.회원수정/삭제

---
+++ basic_syntax/2.basic_command.sh
@@ -0,0 +1,15 @@
+# 현재 working directory, staging area 상태 확인
+git status
+
+Untracked files: # 아직 add 되지 않은 파일들
+
+# .은 모든 수정/추가 사항 add
+git add .
+# 특정파일만 add할 경우
+git add testfolder/test1.txt
+
+# commit을 통해 메시지타이틀과 메시지내용을 
+git commit -m "메시지제목" -m "메시지내용"
+# git commit만 입력하고 엔터시 vi모드 -> 첫줄:타이틀, 두번째줄~:내용
+git commit 
+
