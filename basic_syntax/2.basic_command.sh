c03d0e5b1456ceb254d5e6b55c34cf100c176a68
(hyungkey) <(hyungkey0601@gmail.com)>
2025-06-04 12:21:27 +0900

    12:21

--- basic_syntax/2.basic_command.sh
+++ basic_syntax/2.basic_command.sh
@@ -42,4 +42,10 @@ git diff commitID(A) commitID(B)
 git diff commitID1 commitID2
 git diff 브랜치1 브랜치2 # 많이 쓰임
 
+# 가장 최신의 커밋 취소
+git reset head~1
+git reset head^
+
+# 이미 push된 commit사항 되돌리는(완전한 취소x) commitID생성
+git revert 커밋ID
 
